187411b999aa192199ca542c5631f5db737df5db
Herbert Henrique <herbertmatos10@hotmail.com>
2022-12-27 22:54:02 -0300

    test: ensure SignUpController returns an error if confirmation password is no provided

--- src/presentation/conttollers/signup.spec.ts
+++ src/presentation/conttollers/signup.spec.ts
@@ -35,7 +35,7 @@ describe('SignUp Controller', () => {
     const httpRequest = {
       body: {
         name: 'any_name',
-        email: "any_email@mail.com",
+        email: 'any_email@mail.com',
         confirmationPassword: 'any_password'
       }
     }
@@ -44,4 +44,17 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 
+  test('Should return 400 if no confirmationPassword is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('confirmationPassword'))
+  })
 })
