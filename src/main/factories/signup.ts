dc8ab4c119af39ef030aa931cfde6f7d848cd833
bertdev <herbertmatos10@hotmail.com>
2023-01-08 12:15:13 -0300

    feat: ensure LogControllerDecorator calls controlller handle method

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,14 @@ import { EmailValidatorAdapter } from './../../utils/email-validator-adapter'
 import { DbAddAccountUseCase } from './../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from './../../infra/cryptogaphy/bcrypt-adapter'
 import { AccountMongoRepository } from './../../infra/db/mongodb/account-repository/account'
+import { Controller } from '../../presentation/protocols'
+import { LogControllerDecorator } from './../decorators/log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const bcryptAdapter = new BcryptAdapter()
   const dbAddAccountUseCase = new DbAddAccountUseCase(bcryptAdapter, accountMongoRepository)
   const emailValidator = new EmailValidatorAdapter()
   const signUpController = new SignUpController(emailValidator, dbAddAccountUseCase)
-  return signUpController
+  return new LogControllerDecorator(signUpController)
 }
