12976b0b369b05e3e7cf1a677d0c515555ac8997
bertdev <herbertmatos10@hotmail.com>
2023-01-14 18:01:39 -0300

    test: ensure RequiredFieldValidation returns an error if validation fails and null on success

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,27 @@
+import { RequiredFieldValidation } from './required-field-validation'
+import { MissingParamError } from './../../errors'
+
+interface SutTypes {
+  sut: RequiredFieldValidation
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new RequiredFieldValidation('email')
+  return {
+    sut
+  }
+}
+
+describe('EmailValidation', () => {
+  test('Should return MissingParamError if validation fails', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ name: 'any_name' })
+    expect(error).toBeInstanceOf(MissingParamError)
+  })
+
+  test('Should return null on success', () => {
+    const { sut } = makeSut()
+    const result = sut.validate({ email: 'any_email@mail.com' })
+    expect(result).toBeNull()
+  })
+})
