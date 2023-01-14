c5159263e75b258920d5221f284d9d06db3f3127
bertdev <herbertmatos10@hotmail.com>
2023-01-14 19:15:21 -0300

    feat: ensure login validation factory create ValidationComposite with correct validations

---
+++ src/main/factories/login/login-validation.ts
@@ -0,0 +1,17 @@
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
+import { Validation } from '../../../presentation/helpers/validators/validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+
+export const makeLoginValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  const requiredFields = ['email', 'password']
+  requiredFields.forEach(fieldName => {
+    validations.push(new RequiredFieldValidation(fieldName))
+  })
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const emailValidation = new EmailValidation('email', emailValidatorAdapter)
+  validations.push(emailValidation)
+  return new ValidationComposite(validations)
+}
