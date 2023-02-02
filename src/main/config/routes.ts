ee3ca9bac91df827124731deda1c3b9dd5a8f991
bertdev <herbertmatos10@hotmail.com>
2023-02-01 21:39:39 -0300

    refactor: change routes to use fs instead fast-glob

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,13 @@
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
+import { readdirSync } from 'fs'
+import path from 'path'
 
 export const setupRoutes = (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts').map(async (file): Promise<void> => {
-    const route = (await import(`../../../${file}`)).default
-    route(router)
+  readdirSync(path.join(__dirname, '..', 'routes')).map(async file => {
+    if (!file.includes('.test') && !file.includes('.spec')) {
+      (await import(path.join('..', 'routes', file))).default(router)
+    }
   })
 }
